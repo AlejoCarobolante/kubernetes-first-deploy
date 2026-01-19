59684aac3b8dce45ac343ac8835231ed22ea0c63
Alejo Carobolante <alejo@crea.ar>
2026-01-19 14:54:47 -0300

    Corregido versionado de imagenes de docker y deploys

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
 docker build -t mickey-mouse:latest .
 minikube image load mickey-mouse:latest
 kubectl apply -f todo-k8s.yaml
-kubectl rollout restart deployment/mickey-mouse-deployment
+kubectl rollout restart deployment/hola-mundo-deploy
